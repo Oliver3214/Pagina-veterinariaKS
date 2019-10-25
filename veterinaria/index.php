4a568972a8178f826411bc11372703e7f0ef12e5
oliver <alexander.oliverpes@gmail.com>
2019-10-25 14:41:08 -0600

    Se realizo el codigo html y css, pero no se completo

---
+++ veterinaria/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="estilos.css">
+    <title>VeterinariaKS</title>
+</head>
+<body>
+
+<!--Encabezado-->
+    <header>
+       <nav id="nav" class="nav1" >
+           <div class="contenedor-nav">
+               <div id="lis" class="logo" >
+                   <li class="fab fa-github-square"> </li>
+               </div>
+               <div class="enlaces" id="enlaces">
+                    <a href="#" id="enlaces-inicio" class="btn-header">Inicio</a>
+                    <a href="#" id="enlaces-equipo" class="btn-header">Equipo</a>
+                    <a href="#" id="enlaces-servico" class="btn-header">servicos</a>
+                    <a href="#" id="enlaces-trabajo" class="btn-header">Trabajos</a>
+                    <a href="#" id="enlaces-contacto" class="btn-header">Contacto</a>
+               </div>
+               <div class="icono" id="open">
+                    <span>&#9776;</span>
+               </div>
+           </div>
+       </nav>
+       <div class="textos">
+            <h1>Veterinaria Korky y Stich</h1>
+            <a href="#">Contactanos</a>
+       </div>
+       <div class="barra">
+           <div class="recuadro">
+
+           </div>
+       </div>
+   </header>
+<!--cuerpo de la pagina-->
+    <main>
+<!--conocenos-->
+        <section>
+            <div class="contenedor2">
+            <h1>Conocenos</h1>
+                <div class="grupo-contenedor">
+                    <div class="grup-cont">
+                        <div class="grup-img">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                        <div class="grupo-textos">
+                            <h2>titulo 1</h2>
+                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="grupo-contenedor">
+                    <div class="grup-cont">
+                        <div class="grup-img">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                        <div class="grupo-textos">
+                            <h2>titulo 1</h2>
+                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="grupo-contenedor">
+                    <div class="grup-cont">
+                        <div class="grup-img">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                        <div class="grupo-textos">
+                            <h2>titulo 1</h2>
+                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<!--informacion-->
+        <section>
+            <div class="contenido-sec">
+            <h1>Informacion</h1>
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <h3>titulo1</h3>
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    </div>
+                </div>
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <h3>titulo1</h3>
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    </div>
+                </div>
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <h3>titulo1</h3>
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<!--Galeria-->
+        <section>
+            <div class="galeria">
+            <h1>Nosotros</h1>
+                <h2>titulo central</h2>
+                <div class="cont-galeria">
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="cont-galeria">
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="cont-galeria">
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="person1">
+                        <div class="img-mascota">
+                            <img src="img/fondo2.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<!--Ubicacion-->
+        <section>
+            <div class="ubicacion">
+            <h1>Ubicacion</h1>
+            </div>
+        </section>
+<!--formulario Contactanos-->
+        <section>
+            <div class="Contactanos">
+            <h1>Contactanos</h1>
+            </div>
+        </section>
+    </main>
+<!--pie de pagina-->
+    <footer>
+        <div class="pie-de-pagina">
+            <div>
+
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
