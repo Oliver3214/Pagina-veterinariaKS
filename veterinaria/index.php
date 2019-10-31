0eb354c4898fa252e294935ed07c06f0a9edc2a7
oliver <alexander.oliverpes@gmail.com>
2019-10-31 16:40:05 -0600

    se modifico la pagina completa

--- veterinaria/index.php
+++ veterinaria/index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="estilos.css">
+    <script src="script.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=McLaren|Raleway:100,100i,200,200i,300,300i,400,400i,500,700|Signika&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC|McLaren|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Signika&display=swap" rel="stylesheet">
     <title>VeterinariaKS</title>
 </head>
 <body>
@@ -14,7 +17,7 @@
        <nav id="nav" class="nav1" >
            <div class="contenedor-nav">
                <div id="lis" class="logo" >
-                   <li class="fab fa-github-square"> </li>
+                   <img src="iconos/png/002-huella.png" alt="">
                </div>
                <div class="enlaces" id="enlaces">
                     <a href="#" id="enlaces-inicio" class="btn-header">Inicio</a>
@@ -29,7 +32,8 @@
            </div>
        </nav>
        <div class="textos">
-            <h1>Veterinaria Korky y Stich</h1>
+            <h1>Veterinaria</h1>
+            <h2> Korky y Stich</h2>
             <a href="#">Contactanos</a>
        </div>
        <div class="barra">
@@ -42,128 +46,176 @@
     <main>
 <!--conocenos-->
         <section>
-            <div class="contenedor2">
-            <h1>Conocenos</h1>
-                <div class="grupo-contenedor">
-                    <div class="grup-cont">
-                        <div class="grup-img">
-                            <img src="img/fondo2.jpg" alt="">
-                        </div>
-                        <div class="grupo-textos">
-                            <h2>titulo 1</h2>
-                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
-                        </div>
+            <div class="h1">
+                <h1>Conocenos</h1>
+            </div>
+            <div class="contenido-sec">
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <img src="img/img3.jpg" alt="">
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
                     </div>
                 </div>
-                <div class="grupo-contenedor">
-                    <div class="grup-cont">
-                        <div class="grup-img">
-                            <img src="img/fondo2.jpg" alt="">
-                        </div>
-                        <div class="grupo-textos">
-                            <h2>titulo 1</h2>
-                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
-                        </div>
+                <div class="contenido10">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <img src="img/img3.jpg" alt="">
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
                     </div>
                 </div>
-                <div class="grupo-contenedor">
-                    <div class="grup-cont">
-                        <div class="grup-img">
-                            <img src="img/fondo2.jpg" alt="">
-                        </div>
-                        <div class="grupo-textos">
-                            <h2>titulo 1</h2>
-                            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
-                        </div>
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <img src="img/img3.jpg" alt="">
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
                     </div>
                 </div>
             </div>
         </section>
 <!--informacion-->
         <section>
-            <div class="contenido-sec">
-            <h1>Informacion</h1>
-                <div class="contenido1">
-                    <div class="cont-g">
-                        <div class="titulo1">
-                            <h3>titulo1</h3>
-                        </div class="textos-m1">
-                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+            <div class="mayor1">
+            <h1 >Horarios</h1>
+            <div class="p">
+                <p>Nos especializamos en el bienestar de tu Mascota</p>
+            </div>
+                <div class="contenedor2">
+                    <div class="grupo-contenedor">
+                        <div class="grup-cont">
+                            <div class="grup-img">
+                                <img src="iconos/relojes/png/2.png" alt="">
+                            </div>
+                            <div class="grupo-textos">
+                                <h2>Recomendaciones</h2>
+                                <span>Te recomendamos reservar  </span>
+                                <span> tu cita antes de presentarte</span>
+                                <br>
+                                <span> para evitar inconveniente   </span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="contenido1">
-                    <div class="cont-g">
-                        <div class="titulo1">
-                            <h3>titulo1</h3>
-                        </div class="textos-m1">
-                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    <div class="grupo-contenedor">
+                        <div class="grup-cont">
+                            <div class="grup-img">
+                                <img src="iconos/relojes/png/1.png" alt="">
+                            </div>
+                            <div class="grupo-textos">
+                                <h2>Lunes a viernes</h2>
+                                <span>Atencion al cliente: </span>
+                                <span>de 8:00 am a 5:00 pm </span>
+                                <br>
+                                <span>Todos los dias</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="contenido1">
-                    <div class="cont-g">
-                        <div class="titulo1">
-                            <h3>titulo1</h3>
-                        </div class="textos-m1">
-                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    <div class="grupo-contenedor">
+                        <div class="grup-cont">
+                            <div class="grup-img">
+                                <img src="iconos/relojes/png/3.png" alt="">
+                            </div>
+                            <div class="grupo-textos">
+                                <h2>Urgencias</h2>
+                                <span> Atendemos las 24H habiles, Brindandole el mejor servicio para usted y su mascota
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
 <!--Galeria-->
         <section>
+            <div class="h1">
+                <h1>Nuestras Mascotas</h1>
+            </div>
             <div class="galeria">
-            <h1>Nosotros</h1>
-                <h2>titulo central</h2>
                 <div class="cont-galeria">
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img7.jpg" alt="">
                         </div>
                     </div>
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img8.jpg" alt="">
                         </div>
                     </div>
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img9.jpg" alt="">
                         </div>
                     </div>
                 </div>
-                
+            </div>
+            <div class="galeria">
                 <div class="cont-galeria">
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img4.jpg" alt="">
                         </div>
                     </div>
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img10.jpg" alt="">
                         </div>
                     </div>
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img6.jpg" alt="">
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="galeria">
                 <div class="cont-galeria">
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img1.jpg" alt="">
                         </div>
                     </div>
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img2.jpg" alt="">
                         </div>
                     </div>
                     <div class="person1">
                         <div class="img-mascota">
-                            <img src="img/fondo2.jpg" alt="">
+                            <img src="img/img3.jpg" alt="">
                         </div>
+                    </div>
+                </div>
+            </div>
+<!--seccion Informacion-->
+            <div class="h1">
+                <h1>Informacion</h1>
+            </div>
+            <div class="contenido-sec">
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <img src="img/img3.jpg" alt="">
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    </div>
+                </div>
+                <div class="contenido10">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <img src="img/img3.jpg" alt="">
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
+                    </div>
+                </div>
+                <div class="contenido1">
+                    <div class="cont-g">
+                        <div class="titulo1">
+                            <img src="img/img3.jpg" alt="">
+                        </div class="textos-m1">
+                        <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p></div>
                     </div>
                 </div>
             </div>
@@ -171,23 +223,129 @@
 <!--Ubicacion-->
         <section>
             <div class="ubicacion">
-            <h1>Ubicacion</h1>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61795.29028663241!2d-90.56150159686572!3d14.530226672591214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a7b32deffc89%3A0x2f96cec13bef15de!2sClinica%20Veterinaria%20Pet-kas!5e0!3m2!1ses-419!2sgt!4v1572071263408!5m2!1ses-419!2sgt" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="">
+            </iframe>
             </div>
         </section>
 <!--formulario Contactanos-->
         <section>
             <div class="Contactanos">
-            <h1>Contactanos</h1>
+            <div class="contenedor1234">
+                <div class="conten-form1">
+                    <div class="form1">
+                        <form id="form1234" name="miform" method="POST" action="#" onsubmit="return validarDatos()" >
+                        <div class="h1">
+                            <h2>contactanos</h2>
+                        </div>
+                            <div class="form-grup">
+                                <div class="espacio">
+                                    <input id="nombre" class="cuadro"  type="text" placeholder="Nombre" required>
+                                </div>
+                                <div  class="espacio">
+                                    <input id="email"  class="cuadro" type="email"  placeholder="Email " required>
+                                </div>
+                                <div  class="espacio">
+                                    <select id="selectorEdad" class="cuadro"  required  >
+                                        <option value="">Selecciona tu edad</option>
+                                    </select> 
+                                </div>
+                                <div  class="espacio">
+                                    <select id="sexo" class="cuadro" required>
+                                                <option value="">Selecciona una opcion</option>
+                                                <option value="mujer">Mujer</option>
+                                                <option value="hombre">Hombre</option>
+                                    </select>
+                                </div>
+                                <div  class="espacio">
+                                    <input class="cuadroMensaje"type="mensaje" placeholder="Mensaje"required> 
+                                </div>
+                                <div  class="espacio">
+                                    <input class="checbox" type="checkbox" name="test" id="test" required> <label for="">He leido y acepto las Condiciones y <a href=""> politica de privacidad</a></label>
+                                </div>
+                                <div  class="espacio-enviar">
+                                    <input id="enviar"  type="submit" name="bt1" values="enviar">
+                                </div> 
+                            </div>  
+                        </form>
+                    </div>
+                </div>
+                <div class="conten-form12">
+                    <div class="form1">
+                    </div>
+                    <div>
+                    
+                        <img src="img/img3.jpg" alt="">
+                    </div>
+                </div>
             </div>
         </section>
     </main>
 <!--pie de pagina-->
     <footer>
+        <div class="wave">
+            <div class="style" style="height: 150px; overflow: hidden;" >
+                <svg class="svg" viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;color: fill: #fff;;">
+                    <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff; background: black">
+                     </path>
+                  </svg>
+             </div>
+        </div>
         <div class="pie-de-pagina">
-            <div>
-
-            </div>
+                <div class="contenedor-footer">
+                    <div class="grupo-contenedor-footer">
+                        <div class="grup-cont-footer">
+                            <div class="grup-img-footer">
+                                <img src="iconos/png/m1.png" alt="">
+                            </div>
+                            <div class="grupo-textos -footer">
+                                <h2>titulo 1</h2>
+                                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grupo-contenedor-footer">
+                        <div class="grup-cont-footer">
+                            <div class="grup-img-footer">
+                                <img src="iconos/png/m2.png" alt="">
+                            </div>
+                            <div class="grupo-textos-footer">
+                                <h2>titulo 1</h2>
+                                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grupo-contenedor-footer">
+                        <div class="grup-cont-footer">
+                            <div class="grup-img-footer">
+                                <img src="iconos/png/m3.png" alt="">
+                            </div>
+                            <div class="grupo-textos">
+                                <h2>titulo 1</h2>
+                                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div class="tex-footer">
+                <div class="iconos-redes">
+                    <img src="iconos/redessociales/png/facebook.png" alt="">
+                </div >
+                <div class="iconos-redes">
+                    <img src="iconos/redessociales/png/twitter.png"  alt="">
+                </div >
+                <div class="iconos-redes">
+                    <img src="iconos/redessociales/png/instagram.png"  alt="">
+                </div>
         </div>
     </footer>
+<script >
+ opcionesSelector();
+</script>
+
+<script>
+ validacioncheck()
+</script>
+
 </body>
 </html>
